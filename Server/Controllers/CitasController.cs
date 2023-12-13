4c05dbad33f16369620f0d9ae46bd929a656f8f3
Cucarachita Cosmica <lehi_espr0@hotmail.com>
2023-12-13 13:55:05 -0600

    Cambios en diseño y agregacion de Apis

---
+++ Server/Controllers/CitasController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using QueChulosPerros.Server.Data;
+using QueChulosPerros.Shared.Model;
+
+namespace QueChulosPerros.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CitasController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CitasController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Citas
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Cita>>> GetCitas()
+        {
+          if (_context.Citas == null)
+          {
+              return NotFound();
+          }
+            return await _context.Citas.ToListAsync();
+        }
+
+        // GET: api/Citas/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Cita>> GetCita(int id)
+        {
+          if (_context.Citas == null)
+          {
+              return NotFound();
+          }
+            var cita = await _context.Citas.FindAsync(id);
+
+            if (cita == null)
+            {
+                return NotFound();
+            }
+
+            return cita;
+        }
+
+        // PUT: api/Citas/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCita(int id, Cita cita)
+        {
+            if (id != cita.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(cita).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CitaExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Citas
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Cita>> PostCita(Cita cita)
+        {
+          if (_context.Citas == null)
+          {
+              return Problem("Entity set 'ApplicationDbContext.Citas'  is null.");
+          }
+            _context.Citas.Add(cita);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetCita", new { id = cita.Id }, cita);
+        }
+
+        // DELETE: api/Citas/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCita(int id)
+        {
+            if (_context.Citas == null)
+            {
+                return NotFound();
+            }
+            var cita = await _context.Citas.FindAsync(id);
+            if (cita == null)
+            {
+                return NotFound();
+            }
+
+            _context.Citas.Remove(cita);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool CitaExists(int id)
+        {
+            return (_context.Citas?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ Server/Controllers/MascotasController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using QueChulosPerros.Server.Data;
+using QueChulosPerros.Shared.Model;
+
+namespace QueChulosPerros.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MascotasController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public MascotasController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Mascotas
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Mascota>>> GetMascotas()
+        {
+          if (_context.Mascotas == null)
+          {
+              return NotFound();
+          }
+            return await _context.Mascotas.ToListAsync();
+        }
+
+        // GET: api/Mascotas/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Mascota>> GetMascota(int id)
+        {
+          if (_context.Mascotas == null)
+          {
+              return NotFound();
+          }
+            var mascota = await _context.Mascotas.FindAsync(id);
+
+            if (mascota == null)
+            {
+                return NotFound();
+            }
+
+            return mascota;
+        }
+
+        // PUT: api/Mascotas/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutMascota(int id, Mascota mascota)
+        {
+            if (id != mascota.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(mascota).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!MascotaExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Mascotas
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Mascota>> PostMascota(Mascota mascota)
+        {
+          if (_context.Mascotas == null)
+          {
+              return Problem("Entity set 'ApplicationDbContext.Mascotas'  is null.");
+          }
+            _context.Mascotas.Add(mascota);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetMascota", new { id = mascota.Id }, mascota);
+        }
+
+        // DELETE: api/Mascotas/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteMascota(int id)
+        {
+            if (_context.Mascotas == null)
+            {
+                return NotFound();
+            }
+            var mascota = await _context.Mascotas.FindAsync(id);
+            if (mascota == null)
+            {
+                return NotFound();
+            }
+
+            _context.Mascotas.Remove(mascota);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool MascotaExists(int id)
+        {
+            return (_context.Mascotas?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
