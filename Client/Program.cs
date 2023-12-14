b6bec2e47372c550a72d3a8f01e3eeae81efe0bf
Lehi <lehi_espr0@hotmail.com>
2023-12-13 19:08:54 -0600

    Citas, Loguin, Controllers

--- Client/Program.cs
+++ Client/Program.cs
@@ -4,8 +4,10 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using QueChulosPerros.Client.Authentication;
 using QueChulosPerros.Client;
+using Syncfusion.Blazor; // Añade esta línea
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Ngo9BigBOggjHTQxAR8/V1NHaF1cWWhIfEx0RXxbf1xzZFJMY1tbR3NPIiBoS35RdURhWHxfeXdSRWVdWUJx");
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped<CustomAuthenticationStateProvider>();
@@ -15,5 +17,6 @@ builder.Services.AddBlazoredSessionStorage();
 builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthenticationStateProvider>();
 builder.Services.AddAuthorizationCore();
 
+builder.Services.AddSyncfusionBlazor(); // Añade esta línea
 
 await builder.Build().RunAsync();
